a11a646639dad3fdf7135c1f8bdf53c26ab890f3
raphaelagil <raphaela.gil@yahoo.com>
2024-01-20 15:37:41 +0000

    added the select options for dropdown btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,53 @@
-<<<<<<< HEAD
-let userInputIngredients = $("#userData").val().trim();
-=======
 // Empty array of previous searches, needed for buttons and localstorage
 let userIngredientsSearch = [];
 
+let userInputIngredients = $("#userData").val();
 
->>>>>>> f2c73accc96cc4a728297e4b230b2bb05ea5612c
-function getInfo() {
-
-  $("#search-form").on("submit", function (e) {
-    e.preventDefault();
-
-
+function getInfo(ingredient) {
+   
     //APi for the food search conform the ingredients 
     //!Test the input!
-    
     const apiKeySearch = "129bd2c702ab43ebbdcd90c506ff5b5c";
+    // const apiKeySearch = "20fa1c17de69490f93632c908260c7bb";
+    // const apiKeySearch = "2577d71950714c1ba832e6d94daa3f21";
 
     // const queryUrlSearch = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${userInputIngredients}&number=4&apiKey=${apiKeySearch}`;
-
-    const queryUrlSearch = `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${userInputIngredients}&addRecipeInformation=true&fillIngredients=true&number=4&apiKey=${apiKeySearch}`
+    const queryUrlSearch = `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${ingredient}&addRecipeInformation=true&fillIngredients=true&number=4&apiKey=${apiKeySearch}`
 
     fetch(queryUrlSearch)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
+        // console.log(data);
 
-        renderButton(userInputIngredients)
-
+        renderButton(ingredient)
         recipesCards(data)
-
         $(`#userData-input`).val(``);
 
       });
-  });
-}
+  }
 
-//API for the food Nutrition
-// !Test the input
-const input = $('.ingredients').val()
-const foodSearch = $('input')
-
-<<<<<<< HEAD
-const apiKeyNutrition = '3d3be652dc9fb5eed687451afb2224d5';
-const apiIdNutrition = '3b7c2557';
-=======
-// This function capitalize any string parameter will be passed in. It makes sure that each input ingredient from the user will be capitalized and then used for the name of the buttons (this is happening in the renderButton function).
-function capitalizeWords(inputString) {
-  return inputString.replace(/\b\w/g, function(char) {
-    return char.toUpperCase();
-  });
-}
+  function userInput() {
+    $("#search-form").on("submit", function (e) {
+      e.preventDefault();
+  
+      let userInputIngredients = $("#userData").val().trim();
+  
+      getInfo(userInputIngredients)
+    })
+  }
+  
+  userInput()
+  
+  
+  // This function capitalize any string parameter will be passed in. It makes sure that each input ingredient from the user will be capitalized and then used for the name of the buttons (this is happening in the renderButton function).
+  function capitalizeWords(inputString) {
+    return inputString.replace(/\b\w/g, function(char) {
+      return char.toUpperCase();
+    });
+  }
+  
 
 
 // This function creates buttons for each user search and it checks if the button with the same ingredient already exist, if not it will append it to the aside section.
@@ -69,30 +64,33 @@ function renderButton(userInputIngredients) {
   }
 }
 
->>>>>>> f2c73accc96cc4a728297e4b230b2bb05ea5612c
 
-const queryUrl = `https://api.edamam.com/api/nutrition-data?app_id=${apiIdNutrition}&app_key=${apiKeyNutrition}&nutrition-type=cooking&ingr=${foodSearch}`;
+//API for the food Nutrition
+// !Test the input
+function nutrition(){
+
+const ingredientsNutrition = `${data.ingredients}`
+
+const apiKeyNutrition = '3d3be652dc9fb5eed687451afb2224d5';
+const apiIdNutrition = '3b7c2557';
+
+// const apiKeyNutrition = '8ac12198fbdb382b08155c59b542c40f';
+// const apiIdNutrition = 'c3a22b69';
+
+// const apiKeyNutrition = '3d3be652dc9fb5eed687451afb2224d5';
+// const apiIdNutrition = '3b7c2557';
+
+const queryUrl = `https://api.edamam.com/api/nutrition-data?app_id=${apiIdNutrition}&app_key=${apiKeyNutrition}&nutrition-type=cooking&ingr=${ingredientsNutrition}`;
 
 fetch(queryUrl)
   .then(function (response) {
     return response.json();
   })
-  .then(function (data) {
-    console.log(data);
+  .then(function (dataIngredients) {
+    console.log(dataIngredients);
+ });
+  }
 
-    const file = $('.container')
-    searchBtn.on('click', function (e) {
-      e.preventDefault();
-
-      const nutrition = `${data.ingredients}`
-
-<<<<<<< HEAD
-    });
-  })
-
-
-=======
->>>>>>> f2c73accc96cc4a728297e4b230b2bb05ea5612c
   function recipesCards(data) {
     $(`.food-options`).empty()
     for (let i = 0; i < data.results.length; i++) {
