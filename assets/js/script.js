7db27027218d4dbea3f89c57ed9202518173c081
raphaelagil <raphaela.gil@yahoo.com>
2024-01-21 17:18:35 +0000

    new push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,15 @@
-<<<<<<< HEAD
-let userInputIngredients = $("#userData").val().trim();
-=======
 // Empty array of previous searches, needed for buttons and localstorage
 let userIngredientsSearch = [];
 
+let userInputIngredients = $("#userData").val();
 
->>>>>>> f2c73accc96cc4a728297e4b230b2bb05ea5612c
-function getInfo() {
-
-  $("#search-form").on("submit", function (e) {
-    e.preventDefault();
-
-
+function getInfo(ingredient) {
+   
     //APi for the food search conform the ingredients 
-    const apiKeySearch = "2577d71950714c1ba832e6d94daa3f21";
+    //!Test the input!
+    const apiKeySearch = "129bd2c702ab43ebbdcd90c506ff5b5c";
+    // const apiKeySearch = "20fa1c17de69490f93632c908260c7bb";
+    // const apiKeySearch = "2577d71950714c1ba832e6d94daa3f21";
 
     const queryUrlSearch = `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${ingredient}&addRecipeInformation=true&fillIngredients=true&number=4&apiKey=${apiKeySearch}`
 
@@ -22,36 +18,35 @@ function getInfo() {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
+        // console.log(data);
 
         renderButton(ingredient)
-
         recipesCards(data)
-
         $(`#userData-input`).val(``);
 
       });
-}
+  }
 
-function userInput() {
-  $("#search-form").on("submit", function (e) {
-    e.preventDefault();
-
-    let userInputIngredients = $("#userData").val().trim();
-
-    getInfo(userInputIngredients)
-  })
-}
-
-userInput()
-
-
-// This function capitalize any string parameter will be passed in. It makes sure that each input ingredient from the user will be capitalized and then used for the name of the buttons (this is happening in the renderButton function).
-function capitalizeWords(inputString) {
-  return inputString.replace(/\b\w/g, function(char) {
-    return char.toUpperCase();
-  });
-}
+  function userInput() {
+    $("#search-form").on("submit", function (e) {
+      e.preventDefault();
+  
+      let userInputIngredients = $("#userData").val().trim();
+  
+      getInfo(userInputIngredients)
+    })
+  }
+  
+  userInput()
+  
+  
+  // This function capitalize any string parameter will be passed in. It makes sure that each input ingredient from the user will be capitalized and then used for the name of the buttons (this is happening in the renderButton function).
+  function capitalizeWords(inputString) {
+    return inputString.replace(/\b\w/g, function(char) {
+      return char.toUpperCase();
+    });
+  }
+  
 
 
 // This function creates buttons for each user search and it checks if the button with the same ingredient already exist, if not it will append it to the aside section.
@@ -68,39 +63,42 @@ function renderButton(userInputIngredients) {
   }
 }
 
->>>>>>> f2c73accc96cc4a728297e4b230b2bb05ea5612c
 
-const queryUrl = `https://api.edamam.com/api/nutrition-data?app_id=${apiIdNutrition}&app_key=${apiKeyNutrition}&nutrition-type=cooking&ingr=${foodSearch}`;
+//API for the food Nutrition
+// !Test the input
+function nutrition(){
+
+const ingredientsNutrition = `${data.ingredients}`
+
+const apiKeyNutrition = '3d3be652dc9fb5eed687451afb2224d5';
+const apiIdNutrition = '3b7c2557';
+
+// const apiKeyNutrition = '8ac12198fbdb382b08155c59b542c40f';
+// const apiIdNutrition = 'c3a22b69';
+
+// const apiKeyNutrition = '3d3be652dc9fb5eed687451afb2224d5';
+// const apiIdNutrition = '3b7c2557';
+
+const queryUrl = `https://api.edamam.com/api/nutrition-data?app_id=${apiIdNutrition}&app_key=${apiKeyNutrition}&nutrition-type=cooking&ingr=${ingredientsNutrition}`;
 
 fetch(queryUrl)
   .then(function (response) {
     return response.json();
   })
-  .then(function (data) {
-    console.log(data);
+  .then(function (dataIngredients) {
+    console.log(dataIngredients);
+ });
+  }
 
-    const file = $('.container')
-    searchBtn.on('click', function (e) {
-      e.preventDefault();
-
-      const nutrition = `${data.ingredients}`
-
-<<<<<<< HEAD
-    });
-  })
-
-
-=======
->>>>>>> f2c73accc96cc4a728297e4b230b2bb05ea5612c
   function recipesCards(data) {
     $(`.food-options`).empty()
     for (let i = 0; i < data.results.length; i++) {
-      const divCard = $(`<div class = "card">`);
-      const divCardBody = $(`<div class= "card-body">`);
+      const divCard = $(`<div class = card>`);
+      const divCardBody = $(`<div class=card-body>`);
       const recipeTitle = $(`<h5>`).text(data.results[i].title);
-      const recipeImg = $(`<img class = "imgRecipe">`).attr(`src`, data.results[i].image);
-      const recipeTime = $(`<p class = "readyInMinutes">`).text(`Ready in ${data.results[i].readyInMinutes} min`)
-      const recipeServing = $(`<p class = "serving">`).text(`Serving: ${data.results[i].servings}`)
+      const recipeImg = $(`<img class = imgRecipe>`).attr(`src`, data.results[i].image);
+      const recipeTime = $(`<p class = readyInMinutes>`).text(`Ready in ${data.results[i].readyInMinutes} min`)
+      const recipeServing = $(`<p class = serving>`).text(`Serving: ${data.results[i].servings}`)
   
       const dietsDiv = $(`<div class = "dietsInfo">`)
       const dietsTitle = $(`<h6>`).text(`Diets:`)
@@ -124,7 +122,7 @@ fetch(queryUrl)
   
       $(`.food-options`).append(divCard);
       divCard.append(divCardBody);
-      divCardBody.append(recipeTitle, recipeImg, recipeTime, recipeServing, dietsDiv, ingredientsDiv)
+      divCardBody.append(recipeTitle, recipeImg, recipeTime,recipeServing, dietsDiv, ingredientsDiv)
       // we could add cusines and link to external URL
     }
   }
